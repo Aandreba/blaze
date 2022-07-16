65f878b175cde3e6ae4b2e089ee9d8e246e9d9c1
alex_andreba <aandreba@seidor.es>
2022-07-16 16:45:30 +0200

    testing rscl macro

--- rscl-proc/tests/arith.cl
+++ rscl-proc/tests/arith.cl
@@ -1,6 +1,5 @@
 kernel void sum (const ulong n, __global const float* rhs, __global const float* in, __global float* out) {
     for (ulong id = get_global_id(0); id<n; id += get_global_size(0)) {
-        int two = (int)in[id];
         out[id] = in[id] + rhs[id];
     }
 }
--- rscl-proc/tests/mod.rs
+++ rscl-proc/tests/mod.rs
@@ -1,16 +1,32 @@
-use rscl::{context::SimpleContext};
+#![feature(allocator_api)]
+
+use core::f32;
+use std::{f32::consts::{PI, E, TAU}, mem::MaybeUninit};
+
+use rscl::{context::SimpleContext, prelude::{Result, Event}, event::WaitList, buffer::{Buffer, flags::MemAccess}};
 use rscl_proc::{global_context, rscl};
 
 #[rscl(Arith)]
 #[link(include_str!("arith.cl"))]
 pub extern "C" {
     #[link_name = "sum"]
-    fn add (len: u64, lhs: *const f32, rhs: *const f32, out: *mut f32);
+    fn add (len: u64, lhs: *const f32, rhs: *const f32, out: *mut MaybeUninit<f32>);
 }
 
 #[global_context]
 static CONTEXT : SimpleContext = SimpleContext::default();
 
 #[test]
-fn test () {
+fn test () -> Result<()> {
+    let arith = Arith::new(None)?;
+    let lhs = Buffer::new(&[1., 2., 3., 4., 5.], MemAccess::default(), false)?;
+    let rhs = Buffer::new(&[PI, E, TAU, 2., -1.], MemAccess::default(), false)?;
+    let mut out = Buffer::<f32>::new_uninit(5, MemAccess::default(), false)?;
+
+    let evt = unsafe { arith.add(5, &lhs, &rhs, &mut out, [5, 1, 1], None, WaitList::EMPTY)? };
+    let (_, duration) = evt.wait_with_duration()?;
+    let out = unsafe { out.assume_init() };
+
+    println!("{out:?}: {duration:?}");
+    Ok(())
 }
