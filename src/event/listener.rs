ebf5dae07bdbfc51d84f20a4aedbf008c83e2826
Alex Andreba <aandrebafreelancer@gmail.com>
2023-01-30 18:03:36 +0100

    require complex event cbs to be sync

--- src/event/listener.rs
+++ src/event/listener.rs
@@ -1,32 +1,37 @@
+use super::{EventStatus, RawEvent};
 use crate::prelude::*;
-use super::{RawEvent, EventStatus};
-use std::{panic::*, sync::{mpsc::{Sender, channel, TryRecvError}}, ffi::c_void};
 use once_cell::unsync::OnceCell;
 use opencl_sys::*;
+use std::{ffi::c_void, panic::*, sync::Arc};
+use utils_atomics::FillQueue;
 
 thread_local! {
-    static LISTENER_THREAD_STATUS : OnceCell<Sender<EventCallback>> = OnceCell::new();
+    static LISTENER_THREAD_STATUS : OnceCell<Arc<FillQueue<EventCallback>>> = OnceCell::new();
 }
 
-pub(super) fn get_sender () -> Sender<EventCallback> {
+pub(super) fn get_sender () -> Arc<FillQueue<EventCallback>> {
     LISTENER_THREAD_STATUS.with(|lts| {
         lts.get_or_init(|| {
-            let (send, recv) = channel::<EventCallback>();
-            
+            let queue = Arc::new(FillQueue::new());
+            let recv = Arc::downgrade(&queue);
+
             std::thread::Builder::new()
                 .name(String::from("Callback Handler"))
                 .spawn(move || {
                     let mut callbacks = Vec::<EventCallback>::with_capacity(1);
-                    let mut open = true;
+                    let mut recv = Some(recv);
 
-                    while open {
-                        loop {
-                            match recv.try_recv() {
-                                Ok(x) => callbacks.push(x),
-                                Err(TryRecvError::Disconnected) => break open = false,
-                                Err(TryRecvError::Empty) => break
+                    loop {
+                        if let Some(ref queue) = recv {
+                            match queue.upgrade() {
+                                Some(queue) => callbacks.extend(queue.chop()),
+                                None => recv = None
                             }
-                        };
+                        }
+
+                        if recv.is_none() && callbacks.is_empty() {
+                            break
+                        }
 
                         let mut i = 0;
                         while i < callbacks.len() {
@@ -36,35 +41,38 @@ pub(super) fn get_sender () -> Sender<EventCallback> {
                                 e @ Err(_) => e,
                                 Ok(_) => {
                                     i += 1;
-                                    continue
+                                    continue;
                                 }
                             };
 
                             let callback = callbacks.swap_remove(i);
                             let v = match callback.cb {
-                                Callback::Boxed(f) => catch_unwind(AssertUnwindSafe(|| f(callback.evt, status))),
+                                Callback::Boxed(f) => {
+                                    catch_unwind(AssertUnwindSafe(|| f(callback.evt, status)))
+                                }
                                 Callback::Raw(f, user_data) => unsafe {
                                     let status = match status {
                                         Ok(x) => x as i32,
-                                        Err(e) => e.ty.as_i32()
+                                        Err(e) => e.ty.as_i32(),
                                     };
-                                    
-                                    catch_unwind(AssertUnwindSafe(|| f(callback.evt.id(), status, user_data)))
-                                }
+
+                                    catch_unwind(AssertUnwindSafe(|| {
+                                        f(callback.evt.id(), status, user_data)
+                                    }))
+                                },
                             };
 
                             if let Err(e) = v {
                                 #[cfg(debug_assertions)]
                                 resume_unwind(e);
                                 #[cfg(not(debug_assertions))]
-                                todo!()
+                                eprintln!("{e}")
                             }
                         }
                     }
-                    
-                }).unwrap();
-
-            return send
+                })
+                .unwrap();
+            return queue
         }).clone()
     })
 }
@@ -72,12 +80,15 @@ pub(super) fn get_sender () -> Sender<EventCallback> {
 pub(super) struct EventCallback {
     pub evt: RawEvent,
     pub status: EventStatus,
-    pub cb: Callback
+    pub cb: Callback,
 }
 
 pub(super) enum Callback {
-    Boxed (Box<dyn FnOnce(RawEvent, Result<EventStatus>) + Send>),
-    Raw (unsafe extern "C" fn(event: cl_event, event_command_status: cl_int, user_data: *mut c_void), *mut c_void)
+    Boxed(Box<dyn FnOnce(RawEvent, Result<EventStatus>) + Send + Sync>),
+    Raw(
+        unsafe extern "C" fn(event: cl_event, event_command_status: cl_int, user_data: *mut c_void),
+        *mut c_void,
+    ),
 }
 
 unsafe impl Send for Callback {}
